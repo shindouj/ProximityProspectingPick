fe223c051797d3ac667ca0b24bd593f508318892
jeikobu__ <github@jeikobu.net>
2024-05-06 19:32:52 +0200

    Release 1.0.1

--- ProximityProspectingPick/Properties/AssemblyInfo.cs
+++ ProximityProspectingPick/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using Vintagestory.API.Common;
 [assembly: ComVisible(false)]
 [assembly: Guid("55d051b3-13e7-41d2-833d-b02a501b8050")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 
 [assembly: ModInfo("Proximity Prospecting Pick Mode", "proximityprospectingpick",
-    Version = "1.0.0",
+    Version = "1.0.1",
     Description = "Adds a third prospecting pick mode allowing you to see the distance to closest ore block.",
     Authors = new[] { "Shindou Jeikobu" }
 )]
